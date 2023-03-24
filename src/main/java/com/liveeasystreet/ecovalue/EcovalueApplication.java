780aa9bdcde7efadcc1556eb91f3b11f8d1a53d2
Mulmer <mulmer663@gmail.com>
2023-03-24 19:17:58 +0900

    First Commit

---
+++ src/main/java/com/liveeasystreet/ecovalue/EcovalueApplication.java
@@ -0,0 +1,13 @@
+package com.liveeasystreet.ecovalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcovalueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcovalueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liveeasystreet/ecovalue/EcovalueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveeasystreet.ecovalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcovalueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
