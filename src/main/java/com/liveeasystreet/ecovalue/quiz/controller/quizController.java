a187e54015e923be56bc65f9565eafa32119e760
gyoon <xmas6904201@gmail.com>
2023-03-28 16:26:26 +0900

    퀴즈 문제 , 퀴즈 풀이화면 테스트2

--- src/main/java/com/liveeasystreet/ecovalue/quiz/controller/quizController.java
+++ src/main/java/com/liveeasystreet/ecovalue/quiz/controller/quizController.java
@@ -1,14 +1,13 @@
 package com.liveeasystreet.ecovalue.quiz.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/quiz")
 @Controller
 public class quizController {
 
-    @RequestMapping("/quiz")
+    @RequestMapping
     public String quizStarter() {
         return "/quiz/QuizMain";
     }
@@ -17,4 +16,10 @@ public class quizController {
     public String question() {
         return "/quiz/QuizQuestion";
     }
+
+    @RequestMapping("/addQuiz")
+    public String quizCreate() {
+        return "/quiz/QuizCreate";
+    }
+
 }
