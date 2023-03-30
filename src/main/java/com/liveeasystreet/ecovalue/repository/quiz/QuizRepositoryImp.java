553416fbb96dc2b01a7493c0e9b94d2e966e32b5
geomusteel <geomusteel@naver.com>
2023-03-30 20:06:56 +0900

    :hammer: 퀴즈 리포지토리 구현체 일부 수정
    Issues: #11

--- src/main/java/com/liveeasystreet/ecovalue/repository/quiz/QuizRepositoryImp.java
+++ src/main/java/com/liveeasystreet/ecovalue/repository/quiz/QuizRepositoryImp.java
@@ -2,9 +2,11 @@ package com.liveeasystreet.ecovalue.repository.quiz;
 
 import com.liveeasystreet.ecovalue.domain.Quiz;
 import com.liveeasystreet.ecovalue.dto.QuizDTO;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class QuizRepositoryImp implements QuizRepository {
 
     private static Map<Long, Quiz> store = new HashMap<>();
@@ -46,7 +48,7 @@ public class QuizRepositoryImp implements QuizRepository {
     @Override
     public void update(Quiz quiz) {
         Long id = quiz.getId();
-        Quiz savedQuiz = this.findById(id).orElseGet(null);
+        Quiz savedQuiz = this.findById(id).orElseThrow(() -> new IllegalArgumentException("store 안에 해당 퀴즈가 없습니다."));
         savedQuiz.setOccurredProblemCount(quiz.getOccurredProblemCount());
         savedQuiz.setNumberOfHits(quiz.getNumberOfHits());
     }
