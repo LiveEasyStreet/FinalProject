c605dce4de05047eb26eabc285199b0ed947e204
Mulmer <mulmer663@gmail.com>
2023-03-31 18:06:41 +0900

    :bug: 자잘한 버그 및 디렉토리 정리
    Issues: #6

--- src/main/java/com/liveeasystreet/ecovalue/controller/quiz/quizController.java
+++ src/main/java/com/liveeasystreet/ecovalue/controller/quiz/quizController.java
@@ -1,4 +1,4 @@
-package com.liveeasystreet.ecovalue.quiz.controller;
+package com.liveeasystreet.ecovalue.controller.quiz;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
