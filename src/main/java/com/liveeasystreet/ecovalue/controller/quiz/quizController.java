149b42d270f5e4465898fc5835a2af8f369d652a
Mulmer <mulmer663@gmail.com>
2023-03-31 18:40:58 +0900

    :hammer: 퀴즈 컨트롤러 구현
    컨트롤러 구현, 서비스 로직 하나 변경, 리포지토리 로직 오류 고침
    
    Issues: #14

--- src/main/java/com/liveeasystreet/ecovalue/controller/quiz/quizController.java
+++ src/main/java/com/liveeasystreet/ecovalue/controller/quiz/quizController.java
@@ -1,30 +1,68 @@
 package com.liveeasystreet.ecovalue.controller.quiz;
 
+import com.liveeasystreet.ecovalue.domain.Quiz;
+import com.liveeasystreet.ecovalue.service.quiz.IQuizService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/quiz")
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class quizController {
 
+    private final IQuizService quizService;
+
+    /**
+     * quiz 메인페이지 연결
+     */
     @GetMapping
     public String quizMain() {
-        log.info("quizStarter invoked");
+//        log.info("quizStarter invoked");
         return "/quiz/QuizMain";
     }
 
+    /**
+     * 서비스의 selectsQuiz 메소드를 호출해 10개의 퀴즈를
+     * 퀴즈 풀이 화면에 전달
+     */
     @GetMapping("/question")
-    public String question() {
-        log.info("question invoked");
+    public String question(Model model) {
+//        log.info("question invoked");
+        List<Quiz> quizList = quizService.selectsQuiz();
+        model.addAttribute(quizList);
         return "/quiz/QuizQuestion";
     }
 
+    /**
+     * 퀴즈 풀고 난 후 JS에서 전달한 Map 형식의 퀴즈 오답내역인 quizData를 받은 후
+     * 서비스의 후처리 메소드를 호출 해 통계 정보를 업데이트 한 뒤
+     * 점수 화면에 점수, 퀴즈 10개, 퀴즈 오답내역을 전달
+     */
     @GetMapping("/score")
-    public String score() {
-        log.info("score invoked");
+    public String score(@ModelAttribute Map<Long, Boolean> quizData, Model model) {
+//        log.info("score invoked");
+        int score = quizService.updateQuizStatistics(quizData);
+
+        Map<Long, Quiz> quizMap = new HashMap<>();
+        for (Long key : quizData.keySet()) {
+            Quiz quiz = quizService.findQuiz(key);
+            quizMap.put(quiz.getId(), quiz);
+        }
+
+        model.addAttribute(score);
+        model.addAttribute(quizMap);
+        model.addAttribute(quizData);
+        
         return "/quiz/Score";
     }
 }
