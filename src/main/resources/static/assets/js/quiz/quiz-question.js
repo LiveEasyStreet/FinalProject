ebe6ef3af0887043a7b6661e580f11771bbd96c7
Mulmer <mulmer663@gmail.com>
2023-04-03 23:25:11 +0900

    :hammer: 퀴즈 JS 구현2
    태경씨 안 푼 퀴즈 체크 js 로직 넣었고 submit용 버튼 한 개 더 추가
    
    Issues: #12

--- src/main/resources/static/assets/js/quiz/quiz-question.js
+++ src/main/resources/static/assets/js/quiz/quiz-question.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         type: "fraction",
@@ -19,3 +19,52 @@ oButton.addEventListener('click', () => {
 xButton.addEventListener('click', () => {
     swiper.slideNext();
 })
+
+let counted_quiz = 1;
+const Quiz_User_Submit = {
+    1: [false, false],
+    2: [false, false],
+    3: [false, false],
+    4: [false, false],
+    5: [false, false],
+    6: [false, false],
+    7: [false, false],
+    8: [false, false],
+    9: [false, false],
+    10: [false, false]
+
+}
+
+let plusNumber = (value) => {
+    if (value === 'O' || value === 'X') {
+        console.log(value, swiper.activeIndex);
+        if (value === 'O') {
+            Quiz_User_Submit[swiper.activeIndex + 1][0] = true;
+            if (Quiz_User_Submit[swiper.activeIndex + 1][1] == false) {
+                Quiz_User_Submit[swiper.activeIndex + 1][1] = true;
+            }
+        }
+        else {
+            Quiz_User_Submit[swiper.activeIndex + 1][0] = false;
+            if (Quiz_User_Submit[swiper.activeIndex + 1][1] == false) {
+                Quiz_User_Submit[swiper.activeIndex + 1][1] = true;
+            }
+        }
+        // console.log("위치 : ",counted_quiz, "값 : ",Quiz_User_Submit[swiper.activeIndex+1][0], "변환 여부 : ",Quiz_User_Submit[swiper.activeIndex+1][1]);
+    }
+
+}
+
+let quiz_submit = () => {
+    let submit_index = new Array();
+    for (let i = 1; i <= 10; i++) {
+        if (Quiz_User_Submit[i][1] == false) {
+            submit_index.push(i);
+        }
+    }
+    if (submit_index.length > 0) {
+        alert("모든 문제를 푸셔야 제출할 수 있습니다. \n" +
+            "안푼 문제 : " + submit_index.join(' '));
+    }
+
+}
