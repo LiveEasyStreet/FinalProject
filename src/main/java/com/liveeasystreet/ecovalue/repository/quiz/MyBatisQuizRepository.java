982a350b30aadd8997a9d9fd5750433f921b0749
TaeKyoung108 <rlaxorud0108@naver.com>
2023-04-14 19:27:08 +0900

    :recycle:  : 미업로드 부분 재업로드
    Issues: #65

---
+++ src/main/java/com/liveeasystreet/ecovalue/repository/quiz/MyBatisQuizRepository.java
@@ -0,0 +1,61 @@
+package com.liveeasystreet.ecovalue.repository.quiz;
+
+import com.liveeasystreet.ecovalue.domain.Quiz;
+import com.liveeasystreet.ecovalue.dto.QuizDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Primary
+@Repository
+@AllArgsConstructor
+public class MyBatisQuizRepository implements QuizRepository{
+
+    private final QuizMapper quizMapper;
+
+    @Override
+    public void save(Quiz quiz) {
+        quizMapper.save(quiz);
+    }
+
+    @Override
+    public List<Quiz> findAll(QuizSearchCond cond) {
+        return quizMapper.findAll(cond);
+    }
+
+    @Override
+    public List<Long> findAllKeyList() {
+        return quizMapper.findAllKeyList();
+    }
+
+    @Override
+    public Optional<Quiz> findById(Long id) {
+        return quizMapper.findById(id);
+    }
+
+    @Override
+    public void update(Long id, QuizDto updateParam) {
+        quizMapper.update(id,updateParam);
+    }
+
+    //아직 미구현
+    @Override
+    public void update(Quiz quiz){
+        ;
+    }
+
+    @Override
+    public void update(Long id, Boolean score) {
+        quizMapper.updateScore(id,score);
+    }
+
+    @Override
+    public void delete(Long id) {
+        quizMapper.delete(id);
+    }
+}
